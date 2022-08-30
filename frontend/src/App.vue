f67ee5475365abdcff9a93f6474a94a17904a800
Davi ferreira de sena <davifsena2@gmail.com>
2022-08-30 16:38:55 -0300

    Feat: Criando componente header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,32 @@
 <template>
-  
+    	
+    <Header/>
+
 </template>
 
+<script>
+
+    import Header from '@/components/Header.vue';
+
+    export default{
+        name: "App",
+        components: {
+            Header
+        }
+    }
+
+</script>
+
+<style>
+
+    :root{
+        --main-color: rgb(0,0,0);
+        --secundary-color: rgb(255,255,255);
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <header>
+
+    </header>
+
+</template>
+  
+<script>
+
+export default {
+    name: 'Header'
+}
+
+</script>
+
+<style scoped>
+
+    header {
+      width: 100vw;
+      height: 9vh;
+      background-color: var(--main-color);
+    }
+
+</style>
