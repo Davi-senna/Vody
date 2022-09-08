83187311ea56d9ba21cdae7c38c3d59048f6ec25
Davi ferreira de sena <davifsena2@gmail.com>
2022-09-08 10:08:11 -0300

    Feat: Criando migrations

--- backend/database/migrations/2022_09_08_120243_create_category_table.php
+++ backend/database/migrations/2022_09_08_120243_create_category_table.php
@@ -13,15 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('user');
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('category');
     }
 };
