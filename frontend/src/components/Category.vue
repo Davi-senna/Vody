17f5c590d6be15ca943f8c62b1215e9251e5bee3
WCT\davi.sena <davi.sena@callflex.net.br>
2023-05-17 13:53:24 -0300

    feat: Header of category-reports created

---
+++ frontend/src/components/Category.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="category">
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    name: "Category",
+
+}
+
+    </script>
+
+<style scoped>
+
+
+
+</style>
--- frontend/src/components/CategoryReports.vue
+++ frontend/src/components/CategoryReports.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="category-reports">
-
+        <div id="header-category-reports">
+            <span id="title-category">Categorias</span>
+            <span id="button-add-category">Adicionar categoria +</span>
+        </div>
     </div>
 </template>
 
@@ -22,6 +25,26 @@ export default {
     border: 2px solid var(--main-color);
     border-radius: 12px;
     margin-top: 3.5%;
+    padding: 3%;
+}
+
+#header-category-reports{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#title-category{
+    font-size: 20px;
+    font-family: 'Ubuntu', sans-serif;
+    color: var(--main-color);
+}
+
+#button-add-category{
+    font-size: 15px;
+    font-family: 'Ubuntu', sans-serif;
+    color: var(--main-color);
 }
 
 </style>
